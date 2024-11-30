75c03d70416982dd8b3c001428d681bc4bf46dc5
BorkoAXT <borkoaxtyt@gmail.com>
2024-11-30 23:16:00 +0200

    Add project files.

---
+++ Scp3114 Spawn/EventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using PlayerRoles;
+
+namespace SCP3114Spawner
+{
+    public class EventHandlers
+    {
+        private static readonly RoleTypeId SCP3114Role = RoleTypeId.Scp3114; 
+
+        public void OnRoundStarted()
+        {
+            
+            if (ShouldSpawnSCP3114())
+            {
+                var availablePlayers = Player.List.Where(p => p.Role.Type == RoleTypeId.None).ToList();
+
+                if (availablePlayers.Count > 0)
+                {
+                    
+                    var selectedPlayer = availablePlayers[UnityEngine.Random.Range(0, availablePlayers.Count)];
+                    selectedPlayer.Role.Set(SCP3114Role, spawn: true);
+                    Log.Info($"SCP-3114 has spawned as {selectedPlayer.Nickname}.");
+                }
+            }
+        }
+
+        private bool ShouldSpawnSCP3114()
+        {
+            
+            return UnityEngine.Random.Range(0f, 100f) < Plugin.Instance.Config.SpawnChance;
+        }
+    }
+}
---
+++ Scp3114 Spawn/Plugin.cs
@@ -0,0 +1,31 @@
+﻿
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System;
+
+namespace SCP3114Spawner
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "SCP3114Spawner";
+        public override string Author => "YourName";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(6, 0, 0);
+
+        public EventHandlers EventHandlers;
+
+        public override void OnEnabled()
+        {
+            EventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Server.RoundStarted += EventHandlers.OnRoundStarted;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= EventHandlers.OnRoundStarted;
+            EventHandlers = null;
+            base.OnDisabled();
+        }
+    }
+}
